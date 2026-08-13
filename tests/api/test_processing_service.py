a8d1084c68bac0a023393842335e9f0bfb75e41a
paul <paulmarketandtech@gmail.com>
2026-08-13 15:57:06 +0200

    moved downloading transcript to a separate methof

--- tests/api/test_processing_service.py
+++ tests/api/test_processing_service.py
@@ -14,18 +14,29 @@ def service_test():
     return TestProcessingService
 
 
-"""
+with open(
+    "./tests/api/yt_20260806_futurumequities_QzTrr-pFSJM_transcript.txt", "r"
+) as file:
+    transcript_test = file.read()
+
+yt_metadata = {
+    "title": "hand-made title",
+    "transcript_text": transcript_test,
+    "uploader_id": "Johny B",
+    "published_date": 20260806,
+    "url": "fake_url",
+    "transcript_file_name": "made up file name",
+}
+
+
 def test_process_youtube_url(service_test):
-    result = service_test.process_youtube_url(
-        "self str", "https://youtu.be/QzTrr-pFSJM?si=duXj9My7ZVynrrGM"
-    )
+    result = service_test.process_youtube_url("self str", yt_metadata)
 
     assert result.success is True
     assert isinstance(result.summary, str)
     # assert result.processing_time_seconds > 10
     assert result.error is None
     assert isinstance(result.metadata["title"], str)
-"""
 
 
 def test_process_youtube_url_invalid_url(service):
