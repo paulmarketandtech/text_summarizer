55cffba52793a0070d13abcc7d60e7e74272dc2b
paul <paulmarketandtech@gmail.com>
2026-08-15 19:14:37 +0200

    removed invalid import for CI

--- tests/api/test_processing_service.py
+++ tests/api/test_processing_service.py
@@ -1,17 +1,11 @@
 import pytest
 
 from src.api.processing_service import ProcessingService
-from src.api.testing_processing_service import TestProcessingService
 
 
 @pytest.fixture
 def service():
     return ProcessingService
-
-
-@pytest.fixture
-def service_test():
-    return TestProcessingService
 
 
 """
@@ -35,8 +29,8 @@ yt_metadata = {
 }
 
 
-def test_process_youtube_url(service_test):
-    result = service_test.process_youtube_url("self str", yt_metadata)
+def test_process_youtube_url(service):
+    result = service.process_youtube_url("self str", yt_metadata)
 
     assert result.success is True
     assert isinstance(result.summary, str)
